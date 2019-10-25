63f6274f023cedb350e2b88a0bfffdfa47704710
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2019-10-25 15:03:59 -0300

    Pruebas con Spring Framework

--- SpringFramework/src/main/java/com/main/spring/AppMain.java
+++ SpringFramework/src/main/java/com/main/spring/AppMain.java
@@ -5,7 +5,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.main.beans.Barcelona;
+import com.main.beans.Camiseta;
 import com.main.beans.Jugador;
+import com.main.beans.Marca;
 
 public class AppMain {
 
@@ -16,9 +18,17 @@ public class AppMain {
 //		----------------------------- Utilizacion de Interfaces --------------------------------
 		
 		Jugador j = (Jugador) appContext.getBean("jugador1");
+		Camiseta c = (Camiseta) appContext.getBean("camiseta");
+		Marca m = (Marca) appContext.getBean("marca");
 		
+		m.setNombre("Adidas");
+		c.setMarca(m);
+		c.setNumero(10);
+		
+		j.setId(1);
 		j.setNombre("messi");
 		j.setEquipo(new Barcelona());
+		j.setCamiseta(c);
 		
 		System.out.println(j.getNombre() + " - " + j.getId() + " - " + j.getEquipo().mostrar() + " - " + j.getCamiseta().getNumero()
 				+  " - " + j.getCamiseta().getMarca().getNombre());
